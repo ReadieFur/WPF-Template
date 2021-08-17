29eda2d3177f77eee03127f1db3eb8cc3cd96688
Tristan Read <60425965+kOFReadie@users.noreply.github.com>
2021-08-17 18:19:48 +0100

    Project setup

--- src/WPF-Template-App/App.xaml.cs
+++ src/WPF-Template-App/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WPFTemplate
+namespace WPF_Template_App
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ src/WPF-Template-Loader/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//I know this loader could cause a security flaw as anyone could just place any unknown dll into the folder and load that up instead of what the user intends.
+
+namespace WPF_Template_Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
