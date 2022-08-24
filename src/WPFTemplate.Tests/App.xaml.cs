5667257c3517c2d22eb85091a02cc180f5c43c3b
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2022-08-24 02:04:24 +0100

    Misc changes

--- src/WPFTemplate.Tests/App.xaml.cs
+++ src/WPFTemplate.Tests/App.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Resources;
 using WPFTemplate.Extensions;
+using WPFTemplate.Tests.Testing;
 
 namespace WPFTemplate.Tests
 {
@@ -10,20 +10,27 @@ namespace WPFTemplate.Tests
     {
         private async void Application_Startup(object sender, StartupEventArgs e)
         {
-#if DEBUG
+#if DEBUG && false
             #region Testing
             //new DataBinding().ShowDialog();
             //new TypeConversion().ShowDialog();
             //SystemParametersExt.GetSystemParametersInfo();
             //SystemFontsExt.GetSystemParametersInfo();
             //new Window1().ShowDialog();
+            //new ElementsBase().ShowDialog();
+
+            await Task.WhenAny(
+                new ElementsBase().ShowAsync(),
+                new ElementsDemo().ShowAsync()
+            );
             #endregion
 #endif
+#if RELEASE || true
             await Task.WhenAny(
                 new ElementsDemo().ShowAsync(),
                 new WindowChromeDemo().ShowAsync()
             );
-
+#endif
             Environment.Exit(0);
         }
     }
